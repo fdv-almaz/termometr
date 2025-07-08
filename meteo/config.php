4737e99b573a566480b8cbe4fea96cb606fa8fcb
Dzmitry Frantskevich <dzmitry.frantskevich@gmail.com>
2025-07-08 11:28:12 +0200

    several fixes in config.php

--- meteo/config.php
+++ meteo/config.php
@@ -1,9 +1,9 @@
 <?php
 $config = array(
   'db_host' => 'localhost',
-  'db_user' => 'meteo',
-  'db_pass' => 'meteo',
-  'db_name' => 'meteo',
+  'db_user' => 'user',
+  'db_pass' => 'pass',
+  'db_name' => 'DBname',
   'ULcorr'  => '-1.5',
   'DOMcorr' => '+0'
 );
